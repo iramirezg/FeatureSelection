fd0f019cb671bd368bec34bd8ce90fc78de6a57a
iramirezg <iramirezg@localhost.localdomain>
2024-01-18 23:34:46 +0100

    Code testing version. Compatibility for multiple case studies tested.

--- FeatureSelector.py
+++ FeatureSelector.py
@@ -7,23 +7,11 @@
 
 import pandas as pd
 import numpy as np
-import sys
-
-target = 'TOT' 
-result = pd.read_csv("tmp.csv",parse_dates=['DateTime'])
-
-#result = dt_data
-
-analytic_features = list()
-analytic_features.append('Load')
-analytic_features.append('Ambient temperature')
-analytic_features.append('Moisture in top oil')
-
 #BEGIN FEATURE SELECTION
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-#from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import AdaBoostRegressor
@@ -33,29 +21,7 @@ from sklearn.metrics import mean_squared_error
 
 import warnings
 warnings.simplefilter('ignore', category=FutureWarning)
-warnings.filterwarnings('ignore', category=FutureWarning)
-
-EstimatorModels = [ RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor ]
-train_sizes = [0.5,0.75]
-param_grid = {}
-param_grid[AdaBoostRegressor.__name__] = {
-    'n_estimators' : [50,100,500], #[10,50,100,200,500]
-    'base_estimator__min_samples_leaf' : [1,3,5,10,20,50],
-    'base_estimator__max_leaf_nodes' : [None,5,10,50],
-    'base_estimator__max_depth' : [None,3,10,20],
-    }
-param_grid[RandomForestRegressor.__name__] = {
-    'n_estimators' : [50,100,200,500], #[10,50,100,200,500]
-    'min_samples_leaf' : [1,3,5,10,20],
-    'max_leaf_nodes' : [None, 3, 10, 20], #[None,3,5,10,20,50],
-    #'max_depth' : [None,3,10,20], #[None,3,5,10,20,50]
-    }
-param_grid[GradientBoostingRegressor.__name__] = {
-    'n_estimators' : [200,500], #[10,50,100,200,500]
-    'min_samples_leaf' : [1,3,5],  #[1,3,5,10,20],
-    'max_leaf_nodes' : [None,10,20], #[None,3,5,10,20,50]
-    'max_depth' : [None,3,10,20], #[None,3,5,10,20,50]
-    }
+#warnings.filterwarnings('ignore', category=FutureWarning)
 
 class FeatureSelector:
     def __init__(self,pddf,target=None,anal_feat=None):
@@ -65,7 +31,7 @@ class FeatureSelector:
         self.features = list(self.X.keys())
         self.features.remove(self.target)
         self.anft = list()
-        self.REPEAT_TIME = 3
+        self.REPEAT_TIME = 2    #Times to repeat FeatureSelection or HyperparameterTuning process.
         if anal_feat is not None:
             for feat in anal_feat:
                 if feat in self.X.keys():
@@ -78,9 +44,9 @@ class FeatureSelector:
         for feat in self.features:
             for featu in self.features[self.features.index(feat):]:
                 pR, pn = pearsonr(self.X[feat],self.X[featu])
-                if (pR > 0.9) and (feat != featu) and not (feat in featuresPR):
+                if (pR > correlationThreshold) and (feat != featu) and not (feat in featuresPR) and not (featu in self.anft):
                     featuresPR.append(featu)
-                    #print('remove ' + featu + ' corr ' + feat  )
+                    print('remove ' + featu + ' corr ' + feat + '  p:' + str(pR)  )
         for feat in featuresPR:
             if feat in self.features:
                 self.features.remove(feat)
@@ -94,7 +60,7 @@ class FeatureSelector:
         min_ranking = 0.0
         self.selected_features = list()
         scores = list()
-        X, y = make_regression(n_samples=len(result), n_features=len(self.features))
+        X, y = make_regression(n_samples=len(self.X), n_features=len(self.features))
         for var in range(len(self.features)):
             X[:, var] = self.X[self.features[var]][:]
         y[:] = self.X[target][:]
@@ -107,11 +73,14 @@ class FeatureSelector:
         X_train, X_test, y_train, y_test = train_test_split(SX, y, train_size=train_size, shuffle=False)
         for i in range(self.REPEAT_TIME):
             if param is None:
-                model_est = self.model()
+                estimator = self.model()
             else:
-                model_est = self.model(**param)
-            model_est = model()
-            rfe = RFE(estimator=model_est,
+                if self.model is AdaBoostRegressor:
+                    estimator = self.model(base_estimator=DecisionTreeRegressor())
+                    estimator.set_params(**param)
+                else:
+                    estimator = self.model(**param)      
+            rfe = RFE(estimator=estimator,
                       n_features_to_select=1)
             rfe.fit(X_train, y_train)
             ranking = rfe.ranking_
@@ -149,13 +118,18 @@ class FeatureSelector:
         if param is None:
             Tmodel = self.model()
         else:
-            Tmodel = self.model(**param)
-            self.param = param
+            if self.model is AdaBoostRegressor:
+                estimator = self.model(base_estimator=DecisionTreeRegressor())
+                estimator.set_params(**param)
+            else:
+                estimator = self.model(**param)
+            Tmodel = estimator #(**param)
+            #self.param = param
         X, y = make_regression(n_samples=len(self.X), n_features=len(self.selected_features))
         for var in range(len(self.selected_features)):
             X[:, var] = self.X[ self.features[ self.selected_features[var] ] ][:]
         y[:] = self.X[target][:]
-        make_data_standard = True
+        make_data_standard = False
         if make_data_standard:
             scaler=StandardScaler()
             SX = scaler.fit_transform(X, y)
@@ -165,18 +139,18 @@ class FeatureSelector:
         Tmodel.fit(X_train,y_train)
         y_pred = Tmodel.predict(X_test)
         self.estimator = Tmodel
-        print('MAE test data [numbus of features:', X_train.shape[1],']: ' , mean_squared_error(y_test, y_pred))
+        print('MAE test data [number of features:', X_train.shape[1],']: ' , mean_squared_error(y_test, y_pred))
         return Tmodel, y_pred
         
-    def GridSearch(self,param_grid=None):
+    def HyperparamTune(self,param_grid=None):
         if param_grid is None:
             return None
-        self.grid_params = list()
+        grid_params = list()
         X, y = make_regression(n_samples=len(self.X), n_features=len(self.selected_features))
         for var in range(len(self.selected_features)):
             X[:, var] = self.X[ self.features[ self.selected_features[var] ] ][:]
         y[:] = self.X[target][:]
-        make_data_standard = True
+        make_data_standard = False
         if make_data_standard:
             scaler=StandardScaler()
             SX = scaler.fit_transform(X, y)
@@ -184,17 +158,21 @@ class FeatureSelector:
             SX = X
         X_train, X_test, y_train, y_test = train_test_split(SX, y, train_size=self.train_size, shuffle=False)
         for i in range(self.REPEAT_TIME):
-            grid = GridSearchCV(self.model(),param_grid,#verbose=2,
+            if self.model is AdaBoostRegressor:
+                estimator = self.model(base_estimator=DecisionTreeRegressor())
+            else:
+                estimator = self.model()
+            grid = GridSearchCV(estimator,param_grid,
                                 cv=10,
                                 refit=True,
-                                scoring='neg_mean_squared_error',
+                                #scoring='neg_mean_squared_error',
                                 n_jobs=4
                                 )
             grid.fit(X_train,y_train)
-            self.grid_params.append( grid.best_params_ )
-        print(max(self.grid_params,
-                  key=self.grid_params.count))
-        return max(self.grid_params,key=self.grid_params.count)
+            grid_params.append( grid.best_params_ )
+        # print(max(grid_params,
+        #           key=grid_params.count))
+        return max(grid_params,key=grid_params.count)
                                                 
     def printANFT(self):
         print(self.anft)
@@ -204,18 +182,50 @@ class FeatureSelector:
         for feature in self.selected_features:
             print(self.features[feature], ' p:',
                   self.pToBeSelected[feature])
+            
+if __name__ == "__main__": 
+    EstimatorModels = [ AdaBoostRegressor, RandomForestRegressor, GradientBoostingRegressor ]
+    train_sizes = [0.5,0.75]
+    param_grid = {}
+    param_grid[AdaBoostRegressor.__name__] = {
+        'n_estimators' : [50,100,500], #[10,50,100,200,500]
+        'base_estimator__min_samples_leaf' : [1,3,5,10,20,50],
+        'base_estimator__max_leaf_nodes' : [None,5,10,50],
+        'base_estimator__max_depth' : [None,3,10,20],
+        }
+    param_grid[RandomForestRegressor.__name__] = {
+        'n_estimators' : [50,100,200,500], #[10,50,100,200,500]
+        'min_samples_leaf' : [1,3,5,10,20],
+        'max_leaf_nodes' : [None, 3, 10, 20], #[None,3,5,10,20,50],
+        'max_depth' : [None,3,10,20], #[None,3,5,10,20,50]
+        }
+    param_grid[GradientBoostingRegressor.__name__] = {
+        'n_estimators' : [200,500], #[10,50,100,200,500]
+        'min_samples_leaf' : [1,3,5],  #[1,3,5,10,20],
+        'max_leaf_nodes' : [None,10,20], #[None,3,5,10,20,50]
+        'max_depth' : [None,3,10,20], #[None,3,5,10,20,50]
+        }
+    
+    dataframe = pd.read_csv("tmp.csv",parse_dates=['DateTime'])
+    del dataframe['DateTime']  #Meaningless data variable for regression.
+    dataframe.loc[:, (dataframe != dataframe.iloc[0]).any()] #Remove constant columns.
+    
+    dataframe.dropna(axis='columns',how='any',inplace=True)
+    
+    target = 'TOT' 
+    analytic_features = [ 'Load','Ambient temperature','Moisture in top oil' ] #MDTM
+    #analytic_features = [ 'Load','Ambient temperature' ] #IEC
         
-del result['DateTime']  #Meaningless data variable.
-featSel = FeatureSelector(result,target='TOT',anal_feat=analytic_features)
-
-featSel.CorrelationAnalysis()
-removedFeatures = featSel.FeatureSelection(0.5)
-featSel.printSF()
-model, pred = featSel.TrainModel()
-params = featSel.GridSearch(param_grid[RandomForestRegressor.__name__])
-model, pred = featSel.TrainModel(param=params)
-removedFeatures = featSel.FeatureSelection(0.5,param=params)
-featSel.printSF()
-
-sys.exit(0)
-print('###################################################')
+    featSel = FeatureSelector(dataframe,target='TOT',anal_feat=analytic_features)
+    featSel.CorrelationAnalysis()
+    for train_s in train_sizes:
+        for estimator in EstimatorModels:
+            print('TEST ', estimator.__name__, ' train size ', train_s)
+            removedFeatures = featSel.FeatureSelection(train_s,model=estimator)
+            featSel.printSF()
+            model, pred = featSel.TrainModel()
+            params = featSel.HyperparamTune(param_grid[estimator.__name__])
+            print(params)
+            model, pred = featSel.TrainModel(param=params)
+            removedFeatures = featSel.FeatureSelection(train_s,model=estimator,param=params)
+            featSel.printSF()
